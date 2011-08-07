7e1709656a6582a32eb6a11dabc14cfdf65fd11c
maul.esel <maul.esel@go4more.de>
2011-08-08 00:31:30 +0200

    prepared: template for AHK v1.1 / v2 class

---
+++ AHKModule/ClassTemplate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using ChameleonCoder.Templates;
+using ChameleonCoder.Resources.Interfaces;
+
+namespace AHKModule
+{
+    [ChameleonCoder.CCPlugin]
+    public class ClassTemplate : ITemplate
+    {
+        public string About { get { return "(C) 2011, maul.esel"; } }
+
+        public string Author { get { return "maul.esel"; } }
+
+        public string Description { get { return "creates a new class"; } }
+
+        public ImageSource Icon { get { return new System.Windows.Media.Imaging.BitmapImage(new Uri("pack://application:,,,/AHKModule;component/Icons/version2.png")); } }
+
+        public Guid Identifier { get { return GUID; } }
+
+        public string Name { get { return "AHK class template"; } }
+
+        public string Version { get { return "0.0.0.1"; } }
+
+        public void Initialize() { }
+
+        public void Shutdown() { }
+
+        public Type ResourceType
+        {
+            get { return typeof(ChameleonCoder.ResourceCore.CodeResource); }
+        }
+
+        public string Group
+        {
+            get { return "AutoHotkey v1.1 / v2"; }
+        }
+
+        public IEnumerable<Guid> Languages
+        {
+            get
+            {
+                return new List<Guid>(new Guid[2] { AutoHotkey_L.GUID, AutoHotkey2.GUID });
+            }
+        }
+
+        public IResource Create(IResource parent)
+        {
+
+            return null;
+        }
+
+        public static readonly Guid GUID = Guid.NewGuid();
+    }
+}
