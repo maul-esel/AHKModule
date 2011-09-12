fbc922daa531d0cf947ee27be56f76abe324519f
maul.esel <maul.esel@go4more.de>
2011-09-12 18:54:38 +0200

    more work on syntax highlighting: * fixed some 'bugs' in AHK_L definition * added AHK and AHKv2 definitions * added loading of AHK and AHKv2 definitions

--- AHKModule/AutoHotkey.cs
+++ AHKModule/AutoHotkey.cs
@@ -1,17 +1,50 @@
 ﻿using System;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using ICSharpCode.AvalonEdit.Highlighting;
+using XshdLoader = ICSharpCode.AvalonEdit.Highlighting.Xshd.HighlightingLoader;
 
 namespace AHKModule
 {
     [ChameleonCoder.CCPlugin]
     public class AutoHotkey : ModuleBase
     {
-        public override Guid Identifier { get { return Guid; } }
-        public override string Name { get { return "AutoHotkey"; } }
-        public override ImageSource Icon { get { return new BitmapImage(new Uri("pack://application:,,,/AHKModule;component/Icons/basic.png")).GetAsFrozen() as ImageSource; } }
-        public override string Description { get { return "the language module for the AutoHotkey scripting language"; } }
+        public override string Description
+        {
+            get { return "the language module for the AutoHotkey scripting language"; }
+        }
+
+        public override IHighlightingDefinition Highlighting
+        {
+            get
+            {
+                if (definition == null)
+                {
+                    using (var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("AHKModule.Syntax.AHK.xshd"))
+                    using (var reader = System.Xml.XmlReader.Create(stream))
+                        definition = XshdLoader.Load(reader, HighlightingManager.Instance);
+                }
+                return definition;
+            }
+        }
+
+        public override ImageSource Icon
+        {
+            get { return new BitmapImage(new Uri("pack://application:,,,/AHKModule;component/Icons/basic.png")).GetAsFrozen() as ImageSource; }
+        }
+
+        public override Guid Identifier
+        {
+            get { return Guid; }
+        }
+
+        public override string Name
+        {
+            get { return "AutoHotkey"; }
+        }        
 
         public static readonly Guid Guid = new Guid("{f67fb9f3-ea9c-4140-b960-f47a44b5de56}");
+
+        private static IHighlightingDefinition definition;
     }
 }
--- AHKModule/AutoHotkey2.cs
+++ AHKModule/AutoHotkey2.cs
@@ -1,17 +1,50 @@
 ﻿using System;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using ICSharpCode.AvalonEdit.Highlighting;
+using XshdLoader = ICSharpCode.AvalonEdit.Highlighting.Xshd.HighlightingLoader;
 
 namespace AHKModule
 {
     [ChameleonCoder.CCPlugin]
     public class AutoHotkey2 : ModuleBase
     {
-        public override Guid Identifier { get { return Guid; } }
-        public override string Name { get { return "AutoHotkey v2"; } }
-        public override ImageSource Icon { get { return new BitmapImage(new Uri("pack://application:,,,/AHKModule;component/Icons/version2.png")).GetAsFrozen() as ImageSource; } }
-        public override string Description { get { return "the language module for the AutoHotkey v2 scripting language"; } }
+        public override IHighlightingDefinition Highlighting
+        {
+            get
+            {
+                if (definition == null)
+                {
+                    using (var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("AHKModule.Syntax.AHK_2.xshd"))
+                    using (var reader = System.Xml.XmlReader.Create(stream))
+                        definition = XshdLoader.Load(reader, HighlightingManager.Instance);
+                }
+                return definition;
+            }
+        }
+
+        public override string Description
+        {
+            get { return "the language module for the AutoHotkey v2 scripting language"; }
+        }
+
+        public override ImageSource Icon
+        {
+            get { return new BitmapImage(new Uri("pack://application:,,,/AHKModule;component/Icons/version2.png")).GetAsFrozen() as ImageSource; }
+        }
+
+        public override Guid Identifier
+        {
+            get { return Guid; }
+        }
+
+        public override string Name
+        {
+            get { return "AutoHotkey v2"; }
+        }
+
 
         public static readonly Guid Guid = new Guid("{17176460-6d4b-4cca-b64d-b51ac96ab584}");
+        private static IHighlightingDefinition definition;
     }
 }
