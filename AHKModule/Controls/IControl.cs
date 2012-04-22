c55bc2dc45bd7784518fa0a3d541760d491600ca
maul.esel <maul.esel@go4more.de>
2012-04-22 13:34:59 +0200

    add missing file

---
+++ AHKModule/Controls/IControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AhkModule.Controls
+{
+    interface IControl
+    {
+        string Create(int x, int y, string name);
+    }
+}
